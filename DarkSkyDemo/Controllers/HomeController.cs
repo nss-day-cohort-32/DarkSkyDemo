6b6772cf0991419996a9f24dfcefbad900ef0704
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2019-09-03 17:40:22 -0500

    fetch data from dark sky

---
+++ DarkSkyDemo/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DarkSkyDemo.Models;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+
+namespace DarkSkyDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string _weatherUrl = "https://api.darksky.net/forecast/";
+        private readonly IConfiguration _config;
+
+        public HomeController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var weather = await GetWeatherAsync();
+            return Ok(weather);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private async Task<Weather> GetWeatherAsync()
+        {
+            var key = _config["ApiKeys:DarkSky"];
+            var url = $"{_weatherUrl}{key}/36,-86";
+            var client = new HttpClient();
+            var response = await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var weather = await response.Content.ReadAsAsync<Weather>();
+                return weather;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ DarkSkyDemo/Models/Weather.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DarkSkyDemo.Models
+{
+    public class Weather
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public Currently Currently { get; set; }
+    }
+
+    public class Currently
+    {
+        public string Summary { get; set; }
+        public decimal Temperature { get; set; }
+    }
+}
